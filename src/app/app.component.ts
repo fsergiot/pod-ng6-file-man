f0dcbc3aa178abf891cc7a53da12f240718a108b
Tictapps <tictapps@MacBook-Pro.local>
2019-07-15 17:50:07 -0300

    funcionando sin  css, con node local

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import {ConfigInterface, NodeInterface, TreeModel} from 'ng6-file-man';
 
 @Component({
   selector: 'app-root',
@@ -6,5 +7,34 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.scss']
 })
 export class AppComponent {
-  title = 'pod-ng6-file-man';
+  tree: TreeModel;
+  node: NodeInterface;
+  appLanguage = 'es';
+
+  constructor() {
+    const treeConfig: ConfigInterface = {
+      baseURL: 'http://localhost:8080/',
+      api: {
+        listFile: 'api/files/list',
+        uploadFile: 'api/files/upload',
+        downloadFile: 'api/files/download',
+        deleteFile: 'api/files/remove',
+        createFolder: 'api/files/directory',
+        renameFile: 'api/files/rename',
+        searchFiles: 'api/files/search'
+      },
+      options: {
+        allowFolderDownload: false,
+        showFilesInsideTree: false
+      }
+    };
+
+    this.tree = new TreeModel(treeConfig);
+    this.node = this.tree.nodes;
+  }
+
+  // noinspection JSUnusedLocalSymbols
+  itemClicked(event: any) {
+    // console.log(event);
+  }
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,14 +3,22 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import {FileManagerModule} from 'ng6-file-man';
+import {BrowserAnimationsModule} from '@angular/platform-browser/animations';
+import {FormsModule, ReactiveFormsModule} from '@angular/forms';
+import {HttpClientModule} from '@angular/common/http';
 
 @NgModule({
   declarations: [
     AppComponent
   ],
   imports: [
+    BrowserAnimationsModule,
     BrowserModule,
-    AppRoutingModule
+    FormsModule,
+    ReactiveFormsModule,
+    HttpClientModule,
+    FileManagerModule
   ],
   providers: [],
   bootstrap: [AppComponent]
